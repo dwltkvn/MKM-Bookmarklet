46d056b845382fc27dcc9b3166056776cc39c2ce
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2020-05-10 18:00:53 +0200

    Update MKM-FetchTrendPrice.js
    display if thr card is owned based on card list conatined in variable kdoCardList.
    Prevent multiple fetch.

--- MKM-FetchTrendPrice.js
+++ MKM-FetchTrendPrice.js
@@ -1,90 +1,107 @@
-function openPricePopUp(link)
-{
+function openPricePopUp(link) {
     closePricePopUp();
     var myFloatingDiv = document.createElement("div");
-    myFloatingDiv.setAttribute("ID","KDODIV");
-    myFloatingDiv.setAttribute("style","right: 0px; border: 0px none; height: 370px; position: fixed; width: 270px; overflow: hidden; bottom: -67px;");
-
+    myFloatingDiv.setAttribute("ID", "KDODIV");
+    myFloatingDiv.setAttribute("style", "right: 0px; border: 0px none; height: 370px; position: fixed; width: 270px; overflow: hidden; bottom: -67px;");
     var myIFrame = document.createElement("iframe");
-    myIFrame.setAttribute('src',link+'#tabContent-info');
-    myIFrame.setAttribute('scrolling','yes');
-    myIFrame.setAttribute('style','height: 300px; border: 0px none; width: 1650px; margin-bottom: 0px; margin-left: 24px;');
-
+    myIFrame.setAttribute('src', link + '#tabContent-info');
+    myIFrame.setAttribute('scrolling', 'yes');
+    myIFrame.setAttribute('style', 'height: 300px; border: 0px none;  width: 1650px; margin-bottom: 0px; margin-left: 24px; ');
     myFloatingDiv.appendChild(myIFrame);
     document.body.appendChild(myFloatingDiv);
 }
 
-function closePricePopUp()
-{
+function closePricePopUp() {
     myPreviousFloatingDiv = document.querySelector("#KDODIV");
-    if(myPreviousFloatingDiv) myPreviousFloatingDiv.remove();
+    if (myPreviousFloatingDiv)
+        myPreviousFloatingDiv.remove();
 }
 
-function fetchAll()
-{
+function fetchAll() {
     cardUrls = document.querySelectorAll(".table-body > div > .col-thumbnail");
     var i = 0;
-    for (i = 0; i < cardUrls.length; i++) {
-        fetchCardPrices(i+1);
+    for (i = 0;
+        i < cardUrls.length;
+        i++) {
+        fetchCardPrices(i + 1);
     }
 }
 
-function fetchCardPrices(num)
-{
-    var cardLine = document.querySelector(".table-body > div:nth-child("+num+")");
-    var cardURL = cardLine.querySelector(".col-seller > a").getAttribute("href");
+function fetchCardPrices(num) {
+    var cardLine = document.querySelector(".table-body > div:nth-child(" + num + ")");
+    var cardURL = cardLine.querySelector(".col-seller > a").innerText;
     var sellerPrice = cardLine.querySelector(".price-container > div > div > span").innerText;
-    sellerPrice = sellerPrice.substring( 0, sellerPrice.length -2 ).replace(',','.');
+    sellerPrice = sellerPrice.substring(0 , sellerPrice.length - 2).replace(',' , '.');
     var splat = cardURL.split('/');
-    var cardName = splat[ splat.length-1 ].replace('-s','s');
-
-    var cardCommentElem  = cardLine.querySelector(".item-count");
-    if(cardCommentElem.innerText.includes('|')) return;
+    var cardName = cardURL;
+    cardName = cardName.replace(/ \(.*\)/g, '');
+    cardName = cardName.replace(/ \/ /g, ' ');
+    cardName = cardName.replace(/,/g, '');
+    cardName = cardName.replace(/'/g, '');
+    cardName = cardName.replace(/ /g, '-');
+	
+	var dispo;
+	if( typeof(kdoCardList) == 'undefined' ){
+		dispo = ' ? ';
+	}
+	else{
+		if( kdoCardList.includes(cardURL) ) dispo = ' X ';
+		else dispo = ' + ';
+	}
+	
+    var cardCommentElem = cardLine.querySelector(".item-count");
+    if (cardCommentElem.innerText.includes('|') || cardCommentElem.innerText.includes('...'))
+        return;
     cardCommentElem.innerText = "... fetching ...";
-
-    fetch("https://www.cardmarket.com/en/Magic/Cards/"+cardName)
-                .then(function(response) { return response.text(); })
-                .then(function(html) {
-                        
+    fetch("https://www.cardmarket.com/en/Magic/Cards/" + cardName).then(function (response) {
+        return response.text();
+    }).then(function (html) {
         var parser = new DOMParser();
         var doc = parser.parseFromString(html, "text/html");
-
         var fromPrice = doc.querySelector("#info > div > dl > dd:nth-child(6)").innerText;
         var trendPrice = doc.querySelector("#info > div > dl > dd:nth-child(8)").innerText;
-        var trendPrice2 = trendPrice.substring( 0, trendPrice.length -2 ).replace(',','.');
-
+        var trendPrice2 = trendPrice.substring(0 , trendPrice.length - 2).replace(',' , '.');
         var additionnalInfo = "";
-        if( parseFloat(sellerPrice) <= parseFloat(trendPrice2) ) additionnalInfo = " !["+ (parseFloat(trendPrice2)-parseFloat(sellerPrice)).toFixed(2) +"] ";
-
-        cardCommentElem.innerText = fromPrice + " | " + trendPrice + additionnalInfo;
-    })
-    .catch(function(err) {
-        console.log('Failed to fetch page: ', err); 
+		console.log(sellerPrice);
+		console.log(trendPrice2);
+        if (parseFloat(sellerPrice) <= parseFloat(trendPrice2))
+            additionnalInfo = " ![" + (parseFloat(trendPrice2) - parseFloat(sellerPrice)).toFixed(2) + "] ";
+        cardCommentElem.innerText = "_" + dispo + fromPrice + " | " + trendPrice + additionnalInfo;
+    }).catch(function (err) {
+        console.log('Failed to fetch page: ' + err);
         cardCommentElem.innerText = "---";
     });
 }
 
 var previousButton = document.querySelector("#MKMFetchButton");
-if(previousButton) previousButton.remove();
-
+if (previousButton)
+    previousButton.remove();
 var filterDiv = document.querySelector(".w-100");
 var fetchButton = document.createElement("input");
-fetchButton.setAttribute('type','button');
-fetchButton.setAttribute('value','fetch all');
-fetchButton.setAttribute('id','MKMFetchButton');
-fetchButton.onclick = function(){ fetchAll() };
+fetchButton.setAttribute('type', 'button');
+fetchButton.setAttribute('value', 'fetch all');
+fetchButton.setAttribute('id', 'MKMFetchButton');
+fetchButton.onclick = function () {
+    fetchAll()
+};
 filterDiv.appendChild(fetchButton);
-
 var cardImgs = document.querySelectorAll(".table-body > div > .col-offer > .price-container");
 var i = 0;
-for (i = 0; i < cardImgs.length; i++) {
-    cardImgs[i].setAttribute("id",i+1);
-    cardImgs[i].onmouseover = function() { fetchCardPrices(this.id) };
+for (i = 0;
+    i < cardImgs.length;
+    i++) {
+    cardImgs[i].setAttribute("id", i + 1);
+    cardImgs[i].onmouseover = function () {
+        fetchCardPrices(this.id)
+    };
 }
-
 var cardUrls = document.querySelectorAll(".table-body > div > .col-sellerProductInfo > div > .col-seller > a ");
 var i = 0;
-for (i = 0; i < cardUrls.length; i++) {
+for (i = 0;
+    i < cardUrls.length;
+    i++) {
     var currentUrl = cardUrls[i].getAttribute("href");
-    cardUrls[i].onmouseover = function() { openPricePopUp(this.href) };
+    cardUrls[i].onmouseover = function () {
+        openPricePopUp(this.href)
+    };
 }
