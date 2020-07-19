b0e16a839ca1958928147ab6dfd0b95428dd89a6
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2020-07-19 23:30:14 +0200

    Update MKM-FetchTrendPrice.js
    remove the open price in popup, add possibility to remove from preview cart

--- MKM-FetchTrendPrice.js
+++ MKM-FetchTrendPrice.js
@@ -27,6 +27,26 @@ function highlightCart(username)
 	}
 }
 
+function removeFromCart(username,cardValue)
+{
+	var cardName = cardValue.split(':')[0];
+	var cardPrice = cardValue.split(':')[1];
+	
+	var arrayUID;
+	if( localStorage.getItem("MKMPreviewCart") !== null ) arrayUID = JSON.parse( localStorage.getItem("MKMPreviewCart") );
+	else return;
+	
+	if(arrayUID[username] === undefined) return;
+	//arrayUID[username][cardName] = {};
+	delete arrayUID[username][cardName];
+	localStorage.setItem( "MKMPreviewCart", JSON.stringify(arrayUID) );	
+	
+	Lines.forEach( (L) => {
+			var cardLineName = L.querySelector(".col-seller").innerText;
+			if(cardLineName === cardName) L.style.backgroundColor='LightYellow';
+		} )
+}
+
 function sendToCart(username,cardValue)
 {
 	var cardName = cardValue.split(':')[0];
@@ -39,35 +59,6 @@ function sendToCart(username,cardValue)
 	if(arrayUID[username] === undefined) arrayUID[username] = {};
 	arrayUID[username][cardName] = cardPrice;
 	localStorage.setItem( "MKMPreviewCart", JSON.stringify(arrayUID) );	
-}
-
-function openPricePopUp(link) {
-    closePricePopUp();
-    var myFloatingDiv = document.createElement("div");
-    myFloatingDiv.setAttribute("ID", "KDODIV");
-    myFloatingDiv.setAttribute("style", "right: 0px; border: 0px none; height: 370px; position: fixed; width: 270px; overflow: hidden; bottom: -67px;");
-    var myIFrame = document.createElement("iframe");
-    myIFrame.setAttribute('src', link + '#tabContent-info');
-    myIFrame.setAttribute('scrolling', 'yes');
-    myIFrame.setAttribute('style', 'height: 300px; border: 0px none;  width: 1650px; margin-bottom: 0px; margin-left: 24px; ');
-    myFloatingDiv.appendChild(myIFrame);
-    document.body.appendChild(myFloatingDiv);
-}
-
-function closePricePopUp() {
-    myPreviousFloatingDiv = document.querySelector("#KDODIV");
-    if (myPreviousFloatingDiv)
-        myPreviousFloatingDiv.remove();
-}
-
-function fetchAll() {
-    cardUrls = document.querySelectorAll(".table-body > div > .col-thumbnail");
-    var i = 0;
-    for (i = 0;
-        i < cardUrls.length;
-        i++) {
-        fetchCardPrices(i + 1);
-    }
 }
 
 function fetchCardPrices(num) {
@@ -105,8 +96,6 @@ function fetchCardPrices(num) {
         var trendPrice = doc.querySelector("#info > div > dl > dd:nth-child(8)").innerText;
         var trendPrice2 = trendPrice.substring(0 , trendPrice.length - 2).replace(',' , '.');
         var additionnalInfo = "";
-		console.log(sellerPrice);
-		console.log(trendPrice2);
         if (parseFloat(sellerPrice) <= parseFloat(trendPrice2))
             additionnalInfo = " ![" + (parseFloat(trendPrice2) - parseFloat(sellerPrice)).toFixed(2) + "] ";
         cardCommentElem.innerText = "_" + dispo + fromPrice + " | " + trendPrice + additionnalInfo;
@@ -120,14 +109,7 @@ var previousButton = document.querySelector("#MKMFetchButton");
 if (previousButton)
     previousButton.remove();
 var filterDiv = document.querySelector(".w-100");
-var fetchButton = document.createElement("input");
-fetchButton.setAttribute('type', 'button');
-fetchButton.setAttribute('value', 'fetch all');
-fetchButton.setAttribute('id', 'MKMFetchButton');
-fetchButton.onclick = function () {
-    fetchAll()
-};
-filterDiv.appendChild(fetchButton);
+
 var saveButton = document.createElement("input");
 saveButton.setAttribute('type', 'button');
 saveButton.setAttribute('value', 'Preview Cart');
@@ -150,17 +132,6 @@ for (i = 0;
         fetchCardPrices(this.id)
     };
 }
-var cardUrls = document.querySelectorAll(".table-body > div > .col-sellerProductInfo > div > .col-seller > a ");
-var i = 0;
-for (i = 0;
-    i < cardUrls.length;
-    i++) {
-    var currentUrl = cardUrls[i].getAttribute("href");
-    cardUrls[i].onmouseover = function () {
-        openPricePopUp(this.href)
-    };
-}
-
 
 var userName = document.querySelector("h1").innerText.split('\n')[0];
 var Lines = document.querySelectorAll(".table-body > div");
@@ -173,7 +144,20 @@ Lines.forEach( (L) => {
 	var sellerPrice = L.querySelector(".price-container > div > div > span").innerText;
 	sellerPrice = sellerPrice.substring(0 , sellerPrice.length - 2).replace(',' , '.');
 
-	L.querySelectorAll('div.col-offer > div.actions-container > div.input-group').forEach( (e) => {			
+	L.querySelectorAll('div.col-offer > div.actions-container > div.input-group').forEach( (e) => {	
+
+		var btnRem = document.createElement("button");
+		btnRem.onclick = function () {
+			removeFromCart(userName, this.value);
+			highlightCart(userName);
+		};
+		var textnode = document.createTextNode("-");
+		btnRem.appendChild(textnode); 
+		btnRem.setAttribute('class','btn btn-primary btn-sm rounded');
+		btnRem.setAttribute('type','button');
+		btnRem.setAttribute('value',cardName+':'+sellerPrice);
+		e.appendChild(btnRem);
+		
 		var btn = document.createElement("button");
 		btn.onclick = function () {
 			sendToCart(userName, this.value);
